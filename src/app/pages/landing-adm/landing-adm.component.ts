b59f153eb87f59e7987a17155f578bb855538a8e
Giulia Alciati <giulia.alciati@hotmail.com>
2025-05-26 05:27:45 -0300

    Atualizando páginas Landing e Vendas para retornarem dados reais

--- src/app/pages/landing-adm/landing-adm.component.ts
+++ src/app/pages/landing-adm/landing-adm.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarManagementComponent } from "../../components/sidebar-management/sidebar-management.component";
+import { RelatorioVendasComponent } from "../../components/relatorio-vendas/relatorio-vendas.component";
+import { VendaService, Venda } from '../../services/venda.service';
+import { ProductService } from '../../services/product.service';
+import { CustomersService } from '../../services/customers.service';
 
 @Component({
   selector: 'app-landing-adm',
   standalone: true,
-  imports: [SidebarManagementComponent],
+  imports: [SidebarManagementComponent, RelatorioVendasComponent],
   templateUrl: './landing-adm.component.html',
-  styleUrl: './landing-adm.component.scss'
+  styleUrls: ['./landing-adm.component.scss']
 })
-export class LandingAdmComponent {
+export class LandingAdmComponent implements OnInit {
 
+  totalVendas: number = 0;
+  totalProdutos: number = 0;
+  totalClientes: number = 0; // nova propriedade para clientes
+
+  constructor(
+    private vendaService: VendaService,
+    private productService: ProductService,
+    private customersService: CustomersService // importado o serviço
+  ) {}
+
+  ngOnInit(): void {
+    this.vendaService.listarVendas().subscribe((vendas: Venda[]) => {
+      this.totalVendas = vendas.length;
+    });
+
+    this.productService.listarProduto().subscribe(produtos => {
+      this.totalProdutos = produtos.length;
+    });
+
+    this.customersService.listarCliente().subscribe(clientes => {
+      this.totalClientes = clientes.length;
+    });
+  }
 }
